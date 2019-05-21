78bde6760e21c09db44f7736a978772a865e9a06
xyder <xydr2311@gmail.com>
2019-05-21 07:15:18 +0000

    Customized zsh prompt

--- shell/zsh/global_env.zsh
+++ shell/zsh/global_env.zsh
@@ -26,3 +26,13 @@ export KEYTIMEOUT=1  # kill the delay when pressing ESC
 if command -v pypager >/dev/null; then
   export PAGER=pypager
 fi
+
+export POWERLEVEL9K_MODE="nerdfont-complete"
+export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir virtualenv pyenv vcs vi_mode command_execution_time status)
+export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(root_indicator background_jobs history context ssh time)
+# export POWERLEVEL9K_DISABLE_RPROMPT=true
+export POWERLEVEL9K_VI_INSERT_MODE_STRING="Ins"
+export POWERLEVEL9K_VI_COMMAND_MODE_STRING="Nrm"
+export POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_TIME_FOREGROUND='black'
+POWERLEVEL9K_TIME_BACKGROUND='green'
